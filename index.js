8bb65c9137a4cfa1123080ca86ab5f70d416e0ff
Sunlins Lin <sunlins@sunlins.cn>
2018-03-04 10:52:46 +0800

    npm install --save-dev nodemon,modify index.js add: nodemon index.js.npm run dev：文件修改后自动重启服务

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http=require('http');
 
 var server=http.createServer(function(req,res){
   console.log('Responding to a request.');
-  res.end('<h1>hello,word</h1>');
+  res.end('<h1>hello,word!!!</h1>');
 });
 server.listen(3000);
